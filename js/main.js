a538ae342f810c4cc139a168b6b928a04180cb25
Prafull Epili <prafullepili@gmail.com>
2023-02-06 01:13:50 +0530

    Created collections and methods add,remove,at,get

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
-var Animal = Backbone.Model.extend({
-    walk: function () {
-        console.log("Animal walking...")
-    }
-})
-
-var Dog = Animal.extend({
-    walk: function () {
-        Animal.prototype.walk.apply(this);
-        console.log("Dog walking...")
-    }
+var Song = Backbone.Model.extend();
+var Animal = Backbone.Model.extend();
+var Songs = Backbone.Collection.extend({
+    model: [Song, Animal]
 });
 
-var dog = new Dog()
-dog.walk()
+var songs = new Songs([
+    new Song({ title: "Song 1" }),
+    new Song({ title: "Song 2" }),
+    new Song({ title: "Song 3" }),
+    new Animal({ name: "Cat" }),
+]);
+songs.add(new Song({ title: "Song 4" }))
+
+console.log(songs.models)
+
+var firstSong = songs.at(0)
+var songWithIdC2 = songs.get("c2")
+songs.remove(firstSong)
+songs.remove(songWithIdC2)
+
+console.log(songs.models)
