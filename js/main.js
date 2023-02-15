0448c6d0c315bd790a56d193e37fb3801ba2b72e
Prafull Epili <prafullepili@gmail.com>
2023-02-15 18:16:22 +0530

    viewProject done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,70 @@
-var Song = Backbone.Model.extend();
-var SongView = Backbone.View.extend({
-    render: function () {
-        var template = _.template($("#songTemplate").html());
-        // console.log(template({ title: "Hello" }))
-        var html = template(this.model.toJSON());
-        this.$el.html(html);
+var Vehicle = Backbone.Model.extend({
+    idAttribute: "registrationNumber",
+    urlRoot: "/api/vehicles",
+    validate: function (attrs) {
+        if (!attrs.registrationNumber) {
+            return "Vehicle is not valid";
+        }
+    },
+    start: function () {
+        console.log("Vehicle started");
+    }
+});
 
+// collections
+var Vehicles = Backbone.Collection.extend({
+    Model: Vehicle
+});
+
+
+var Car = Vehicle.extend({
+    start: function () {
+        console.log("Car with registration number " + this.get("registrationNumber") + " started.");
+    }
+});
+
+
+var VehicleView = Backbone.View.extend({
+    tagNumber: "li",
+    className: "vehicle",
+
+    events: {
+        "click .delete": "onDelete",
+    },
+
+    render: function () {
+        var source = $('#vehicleTemplate').html();
+        var template = _.template(source);
+
+        this.$el.html(template(this.model.toJSON()));
+        this.$el.attr("data-color", this.model.get("color"));
+
+        return this;
+    },
+    onDelete: function () {
+        this.remove();
+    }
+});
+
+
+var VehiclesView = Backbone.View.extend({
+    tagName: "ul",
+    render: function () {
+        this.collection.each(function (vehicle) {
+            var vehicleView = new VehicleView({ model: vehicle });
+            this.$el.append(vehicleView.render().$el);
+        }, this);
         return this;
     }
 });
 
-var song = new Song({ title: "Blue in Green", plays: 10000 });
 
-var songView = new SongView({ el: "#container", model: song });
-songView.render();
+var vehicles = new Vehicles([
+    new Car({ registrationNumber: "XL1885", color: "Blue" }),
+    new Car({ registrationNumber: "XL1825", color: "Blue" }),
+    new Car({ registrationNumber: "XL1835", color: "Gray" })
+]);
+
+
+var vehiclesView = new VehiclesView({ collection: vehicles });
+$("#container").html(vehiclesView.render().$el);
